8ee5666e094fce571bc5e862073d9c04ba8e167c
Vikrant Bhat <vik.bhat223@gmail.com>
2020-09-06 21:59:15 +0530

    reload btn functionality added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const videoElement = document.getElementById("video");
 const button = document.getElementById("button");
+const reloadButton = document.getElementById("reload-button");
 
 // Prompt the user to select a media stream, pass to video element, then play
 async function selectMediaStream() {
@@ -23,6 +24,10 @@ button.addEventListener("click", async () => {
 
 	// Reset button
 	button.disabled = false;
+});
+
+reloadButton.addEventListener("click", () => {
+	window.location.reload();
 });
 
 // On Load
